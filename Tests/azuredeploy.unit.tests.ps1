5145f0af42814d42b014b9f7f6802e3f780531c4
Sam Cogan <mail@samcogan.com>
2021-01-24 12:52:33 +0000

    convert to pester 5

--- Tests/azuredeploy.unit.tests.ps1
+++ Tests/azuredeploy.unit.tests.ps1
@@ -9,37 +9,38 @@
     Source: https://github.com/Azure/azure-quickstart-templates/tree/master/201-vmss-automation-dsc
 #>
 
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$template = Split-Path -Leaf $here
 
-$templatesFolder="$here\..\templates"
-$parametersFolder="$here\..\parameters"
-
-$TempValidationRG = "Pester-Validation-RG"
-$location = "West Europe"
-Write-host "rg:$TempValidationRG"
 
 
 
 
 Describe "Template: $template" -Tags Unit {
      BeforeAll {
-         New-AzResourceGroup -Name "Pester-Validation-RG" -Location "West Europe"
+        $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+        $template = Split-Path -Leaf $here
+        
+        $templatesFolder="$here\..\templates"
+        $parametersFolder="$here\..\parameters"
+        
+        $TempValidationRG = "Pester-Validation-RG"
+        $location = "West Europe"
+        Write-host "rg:$TempValidationRG"
+         New-AzResourceGroup -Name $TempValidationRG -Location $location
     }
 
     
     Context "Template Syntax" {
         
         It "Has a JSON template" {        
-            "$templatesFolder\azuredeploy.json" | Should Exist
+            "$templatesFolder\azuredeploy.json" | Should -Exist
         }
         
         It "Has a parameters file" {        
-            "$parametersFolder\azuredeploy.parameters.json" | Should Exist
+            "$parametersFolder\azuredeploy.parameters.json" | Should -Exist
         }
         
         It "Has a metadata file" {        
-            "$templatesFolder\metadata.json" | Should Exist
+            "$templatesFolder\metadata.json" | Should -Exist
         }
 
         It "Converts from JSON and has the expected properties" {
@@ -50,14 +51,14 @@ Describe "Template: $template" -Tags Unit {
             'resources',                                
             'outputs' | Sort-Object 
             $templateProperties = (get-content "$templatesFolder\azuredeploy.json" | ConvertFrom-Json -ErrorAction SilentlyContinue) | Get-Member -MemberType NoteProperty | % Name
-            $templateProperties | Sort-Object | Should Be $expectedProperties
+            $templateProperties | Sort-Object | Should -Be $expectedProperties
         }
         
         It "Creates the expected Azure resources" {
             $expectedResources = 'Microsoft.Storage/storageAccounts' | Sort-Object 
 
             $templateResources = (get-content "$templatesFolder\azuredeploy.json" | ConvertFrom-Json -ErrorAction SilentlyContinue).Resources.type
-            $templateResources | Sort-Object | Should Be $expectedResources 
+            $templateResources | Sort-Object | Should -Be $expectedResources 
         }
        
 
@@ -70,11 +71,11 @@ Describe "Template: $template" -Tags Unit {
             # Complete mode - will deploy everything in the template from scratch. If the resource group already contains things (or even items that are not in the template) they will be deleted first.
             # If it passes validation no output is returned, hence we test for NullOrEmpty
             $ValidationResult = Test-AzResourceGroupDeployment -ResourceGroupName $TempValidationRG -Mode Complete -TemplateFile "$templatesFolder\azuredeploy.json" -TemplateParameterFile "$parametersFolder\azuredeploy.parameters.json"
-            $ValidationResult | Should BeNullOrEmpty
+            $ValidationResult | Should -BeNullOrEmpty
         }
     }
 
      AfterAll {
-         Remove-AzResourceGroup "Pester-Validation-RG" -Force
+         Remove-AzResourceGroup $TempValidationRG -Force
      }
 }
